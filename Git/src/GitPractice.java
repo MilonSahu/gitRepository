c7ba6ca73821fc60233d4f0f8b4ddd421e61d59b
MilonSahu <37011157+MilonSahu@users.noreply.github.com>
2018-03-06 17:14:49 +0530

    3 comnmit

--- Git/src/GitPractice.java
+++ Git/src/GitPractice.java
@@ -4,6 +4,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println("Madhuri is mad");
 		System.out.println("viji is mad");
+		System.out.println("rana is mad");
 
 	}
 
